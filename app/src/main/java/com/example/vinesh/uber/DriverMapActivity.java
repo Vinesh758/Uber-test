401d56a07836c5fddc91dba1b1b8d34529ac24af
vinesh <vineshgoyal1@gmail.com>
2017-12-04 19:04:14 +0530

    test

--- app/src/main/java/com/example/vinesh/uber/DriverMapActivity.java
+++ app/src/main/java/com/example/vinesh/uber/DriverMapActivity.java
@@ -24,6 +24,7 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     LocationRequest mLocationRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
